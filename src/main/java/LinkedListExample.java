032baa3300e6e4b6ad4918e4fbab0a4706b7bc98
Nidhi <nidhidang495@gmail.com>
2024-12-15 00:27:14 +0530

    Linked list

---
+++ src/main/java/LinkedListExample.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+
+        list.add("Nidhi");
+        list.add("Parneet");
+        System.out.println(list);
+
+    }
+}
