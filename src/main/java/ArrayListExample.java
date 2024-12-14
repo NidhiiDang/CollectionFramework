965fa4186c1f2e514a942c42d80b25d13ddd04c2
Nidhi <nidhidang495@gmail.com>
2024-12-15 00:07:49 +0530

    arraylist

--- src/main/java/ArrayListExample.java
+++ src/main/java/ArrayListExample.java
@@ -1,7 +1,5 @@
 import java.util.List;
 import java.util.ArrayList;
-
-
 public class ArrayListExample {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
